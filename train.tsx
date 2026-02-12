a34a3132fdfbb2906bf180f3ef7f8c461f1b7fa2
Xushnudbek76 <abdugafforovxushnudbek17@gmail.com>
2026-02-12 13:23:24 +0900

    fix: task deleted

--- train.tsx
+++
@@ -1,8 +0,0 @@
- const missingNumber = (nums: number[]): number => {
-    const n = nums.length;
-    const e = (n * (n + 1)) / 2;
-    const s = nums.reduce((a, b) => a + b, 0);
-    return e - s;
-}
-
-console.log(missingNumber([3, 0, 1])); 
