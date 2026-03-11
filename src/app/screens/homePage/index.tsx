17a2ddbd0072fd885bdc99ead619c602869293b0
Xushnudbek76 <abdugafforovxushnudbek17@gmail.com>
2026-03-12 08:37:50 +0900

    feat: redux store architecture

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {  useEffect } from "react";
 import ActiveUsers from "./ActiveUsers";
 import Advertisement from "./Advertisement";
 import Events from "./Events";
@@ -7,7 +7,23 @@ import PopularDishes from "./PopularDishes";
 import Statistics from "./Statistics";
  // @ts-ignore
 import "../../../css/home.css";
+import { Dispatch } from "@reduxjs/toolkit";
+import { setPopularDishes } from "./slice";
+import { Product } from "../../../lib/types/product";
+import { useDispatch } from "react-redux";
+
+
+    const actionDispatch = (dispatch: Dispatch) => ({
+        setPopularDishes: (data: Product[]) => dispatch(setPopularDishes(data)),
+    }) 
+
 export default function HomePage() {
+    const { setPopularDishes } = actionDispatch(useDispatch());
+
+    useEffect(() => {
+
+    }, [])
+
     return <div className="homepage">
     <Statistics/>
     <PopularDishes/>
---
+++ src/lib/types/screen.ts
@@ -0,0 +1,14 @@
+import { Member } from "./member";
+import { Product } from "./product";
+
+// React App State
+export interface AppRootState {
+    homePage: HomePageState;
+}
+
+
+export interface HomePageState {
+    popularDishes: Product[];
+    newDishes: Product[];
+    topUsers: Member[];
+}
